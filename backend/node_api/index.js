709035252d73ab882b2e03d9bfebf63b026613b3
robitu <robertoiturralde2009@hotmail.com>
2023-12-01 16:12:00 -0700

    Properly uploaded api endpoint

---
+++ backend/node_api/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const { exec } = require("child_process");
+const cors = require("cors");
+
+const api = express();
+api.use(cors());
+
+api.use((req, res, next) => {
+  res.set("Cache-Control", "no-store");
+  next();
+});
+
+const HOST = "localhost";
+const PORT = 8888;
+
+api.get("/", (req, res) => {
+  res.send("Welcome to this awesome API!");
+});
+
+
+api.get("/run-chainlink-functions-script", (req, res) => {
+  console.log("new version?");
+  exec(
+    "node /home/robitu/hackathon/hackathon-fall-2023/opentruth2/backend/scripts/07_congress_request.js",
+    (error, stdout, stderr) => {
+      console.log(`stdout: ${stdout}`);
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return res.status(500).send("Failed to run test script");
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+      res.send(stdout); // Send stdout as a plain text response
+    }
+  );
+});
+
+// api.post ? how to get value from listener?
+
+api.listen(PORT, () => console.log(`API running at ${HOST}:${PORT}!`));
